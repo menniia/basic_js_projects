783532991f611bf6fd7d9ac07ecb1d738455fd6f
menniia <mennia17@gmail.com>
2023-11-29 00:27:37 +0000

    Initial commit

---
+++ rock-paper-scissors/README.md
@@ -0,0 +1,22 @@
+# Rock Paper Scissors Game
+
+This project is a simple Rock Paper Scissors game implemented in JavaScript. It allows the user to play against the computer.
+
+## How to Play
+
+1. Open the `rock-paper-scissors` folder.
+
+2. Open the `index.html` and `script.js` file in a web browser.
+
+3. Click on one of the buttons to make your move (Rock, Paper, or Scissors).
+
+4. The computer will randomly select its move.
+
+5. The result of the game will be displayed, indicating whether you won, lost, or tied.
+
+## Code Overview
+
+- The game logic is implemented in the `playGame` function in the `script.js` file.
+- The computer's move is randomly generated using the `pickComputerMove` function.
+
+- The result of the game is determined based on the player's move and the computer's move.
---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,51 @@
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+    let computerMove;
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = "Rock";
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = "Paper";
+    } else {
+      computerMove = "Scissors";
+    }
+
+    return computerMove;
+  }
+
+  function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+
+    let result;
+
+    if (playerMove === "Scissors") {
+      if (computerMove === "Scissors") {
+        result = "Tie!";
+      } else if (computerMove === "Paper") {
+        result = "You lose!";
+      } else if (computerMove === "Rock") {
+        result = "You win!";
+      }
+    } else if (playerMove === "Paper") {
+      if (computerMove === "Paper") {
+        result = "Tie!";
+      } else if (computerMove === "Scissors") {
+        result = "You lose!";
+      } else if (computerMove === "Rock") {
+        result = "You win!";
+      }
+    } else if (playerMove === "Rock") {
+      if (computerMove === "Rock") {
+        result = "Tie!";
+      } else if (computerMove === "Paper") {
+        result = "You lose!";
+      } else if (computerMove === "Scissors") {
+        result = "You win!";
+      }
+    }
+
+    alert(
+      `You picked ${playerMove}, Computer picked ${computerMove}, ${result}`
+    );
+  }
