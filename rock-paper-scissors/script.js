b28ac87d422b33d41d9c7fc7fa2792f51e5336f0
menniia <mennia17@gmail.com>
2023-12-12 08:48:55 +0000

    updated code using JSON for data serialization and localStorage for persistent storage

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,13 @@
+let score = JSON.parse(localStorage.getItem('score'));
+
+if (!score) {
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };
+}
+
 function pickComputerMove() {
     const randomNumber = Math.random();
 
@@ -45,7 +55,20 @@ function pickComputerMove() {
       }
     }
 
+    if (result === 'You win!') {
+        score.wins++;
+    } else if (result === 'You lose!') {
+        score.losses++;
+    } else if (result === 'Tie!') {
+        score.ties++;
+    }
+
+   localStorage.setItem('score', JSON.stringify(score));
+
     alert(
-      `You picked ${playerMove}, Computer picked ${computerMove}, ${result}`
+      `You picked ${playerMove}, Computer picked ${computerMove}, ${result}
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
     );
   }
+
+  document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
